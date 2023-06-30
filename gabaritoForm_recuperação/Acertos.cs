c1f8012d1ad506bdb56059192205608397d61f25
Giovanna Vonrondov Bergamasco <vonrondovgiovanna820@gmail.com>
2023-06-30 10:19:58 -0400

    primeiro commit prova recuperacao

---
+++ gabaritoForm_recuperação/Acertos.cs
@@ -0,0 +1,24 @@
+﻿public class Acertos
+{
+    public string nome;
+    public string matricula;
+    public string respostas;
+
+    public int QtdAcertos(string[] respostas)
+    {
+        string[] gabarito = { "A", "B", "C", "E", "A", "D", "D", "B", "C", "E" };
+        int cont = 0;
+
+        for (int i = 0; i < gabarito.Length; i++)
+        {
+            if (gabarito[i] == respostas[i])
+            {
+                cont++;
+            }
+        }
+
+        return cont;
+    }
+
+   
+}
---
+++ gabaritoForm_recuperação/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gabaritoForm_recuperação
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Acertos a = new Acertos();
+            string[] respostas = { $"{tx_questao1.Text}", $"{tx_questao2.Text}", $"{tx_questao3.Text}", $"{tx_questao4.Text}", $"{tx_questao5.Text}", $"{tx_questao6.Text}", $"{tx_questao7.Text}", $"{tx_questao8.Text}", $"{tx_questao9.Text}", $"{tx_questao10.Text}" };
+
+            int resultado = a.QtdAcertos(respostas);
+            lb_acertos.Text = resultado.ToString();
+
+        }
+    }
+}
